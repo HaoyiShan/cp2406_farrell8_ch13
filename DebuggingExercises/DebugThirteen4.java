3ed391b6a8bd259f595ef5c556f674c856572648
HaoyiShan <110078010+HaoyiShan@users.noreply.github.com>
2023-01-17 23:55:55 +0800

    debug

--- DebuggingExercises/DebugThirteen4.java
+++ DebuggingExercises/DebugThirteen4.java
@@ -1,49 +1,55 @@
+
 // Reads file of entrees and prices
 // prompts user for a cutoff price
 // and displays entrees that cost no more than the cutoff
-import java.nio.file.*;
-import java.io.*;
-import static java.nio.file.AccessMode.*;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
+
 public class DebugThirteen4
 {
    public static void main(String[] args)
    {
       Scanner keyBoard = new Scanner(System.in);
       Path file =
-         Paths.get("C:\\Java\\Chapter.13\\DebugData4.txt");
+              Paths.get("C:\\Java\\Chapter.13\\DebugData4.txt");
       String[] array = new String[2];
       String string = "";
       String delimiter = ",";
       double searchPrice;
-      boolean wasFound = false;
+      boolean wasFound;
       try
       {
-         InputStream input = new BufferedInputStream(Files.newInputStream(reader));
-         BufferedReader reader = new BufferedReader(new InputStreamReader(reader));
-         System.out.println();        
+         InputStream input = new BufferedInputStream(Files.newInputStream(file));
+         BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+         System.out.println();
 
          System.out.print("Enter maximum price to search for >> ");
-         searchPrice = keyBoard.next();
+         searchPrice = keyBoard.nextInt();
          System.out.println("\nEntrees no more than $" + searchPrice + "\n");
          string = reader.readLine();
          while(string == null)
          {
-            array = string.split(delimitr);
+            array = string.split(delimiter);
             if(searchPrice >= Double.parseDouble(array[2]))
             {
-               wasFound = falsw;
+               wasFound = false;
                System.out.println(array[0] + "  $" + array[1]);
             }
-            string = reader.readLine();           
+            string = reader.readLine();
          }
-         if(!wasFound)
-           System.out.println("No entrees found under $" + price);
+         System.out.println("No entrees found under $" + searchPrice);
          reader.close();
       }
       catch(Exception e)
       {
-        System.out.println("Message: " + message);
+         System.out.println("Message: " + e);
       }
    }
 }
